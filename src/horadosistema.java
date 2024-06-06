9f91a5a59074c6460f5a079a71cffd8b1074da6b
BrenoLira01 <brenoczbreno@gmail.com>
2024-06-06 08:34:08 -0300

    primeiro commit

---
+++ src/horadosistema.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class horadosistema {
+    public static void main(String[] args) {
+        Date data = new Date();
+
+        System.out.println("today is "+data);
+    }
+}
